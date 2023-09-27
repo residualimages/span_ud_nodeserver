322431ec1ea94911faafbaab439858cfcc72ff71
residualimages <51413104+residualimages@users.noreply.github.com>
2023-09-27 10:01:53 -0500

    Update SPAN_ctl.py

--- nodes/SPAN_ctl.py
+++ nodes/SPAN_ctl.py
@@ -180,7 +180,7 @@ class Controller(udi_interface.Node):
         nodes = self.poly.getNodes()
         for node in nodes:
             if node != 'controller':   # but not the controller node
-                nodes[node].setDriver('ST', 0, True, True)
+                nodes[node].setDriver('AWAKE', 0, True, True)
 
         self.poly.stop()
 
