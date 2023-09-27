407bcc8c6815ee29a2025ba048b939c19e5925b9
residualimages <51413104+residualimages@users.noreply.github.com>
2023-09-27 09:56:03 -0500

    Update SPAN_panel.py

--- nodes/SPAN_panel.py
+++ nodes/SPAN_panel.py
@@ -12,6 +12,8 @@ import http.client
 # Standard Library
 from typing import Optional, Any, TYPE_CHECKING
 
+import xml.etree.ElementTree as ET
+
 LOGGER = udi_interface.LOGGER
 Custom = udi_interface.Custom
 
@@ -21,7 +23,7 @@ This is our Panel device node.
 class PanelNode(udi_interface.Node):
     id = 'panel'
     drivers = [
-            {'driver': 'ST', 'value': 1, 'uom': 2},
+            {'driver': 'TPW', 'value': 0, 'uom': 73},
             {'driver': 'PULSCNT', 'value': 0, 'uom': 56},
             {'driver': 'AWAKE', 'value': 1, 'uom': 2}
             ]
@@ -84,14 +86,24 @@ class PanelNode(udi_interface.Node):
                 currentCount = self.getDriver('PULSCNT')
                 currentCount += 1
                 self.setDriver('PULSCNT', currentCount, True, True)
-                LOGGER.info('Current PULSCNT for polling on {} is {}'.format(self.name,currentCount))
+                #LOGGER.info('Current PULSCNT for polling on {} is {}'.format(self.name,currentCount))
                 
                 self.count += 1
                 #LOGGER.info('Current self.count for polling on {} is {}'.format(self.name,self.count))
               
                 # be fancy and display a notice on the polyglot dashboard
                 # self.poly.Notices[self.name] = '{}: Current polling count is {}'.format(self.name, self.count)
-
+        
+                spanConnection.request("GET", "/api/v1/panel", payload, headers)
+        
+                panelResponse = spanConnection.getresponse()
+                panelData = panelResponse.read()
+                panelData = panelData.decode("utf-8")
+                panelDataAsXml = ET.fromstring(panelData)
+                feedthroughPowerW = panelDataAsXml.find('feedthroughPowerW')
+                feedthroughPowerW = math.ceil(feedthroughPowerW*100)/100
+                self.setDriver('TPW', feedthroughPowerW, True, True)
+            
     def toggle_monitoring(self,val):
         # On startup this will always go back to true which is the default, but how do we restore the previous user value?
         LOGGER.debug(f'{self.address} val={val}')
