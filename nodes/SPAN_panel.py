81214814e6f4574e20fd0de895a8c67fe46666f7
residualimages <51413104+residualimages@users.noreply.github.com>
2023-09-29 19:14:04 -0500

    Update SPAN_panel.py

--- nodes/SPAN_panel.py
+++ nodes/SPAN_panel.py
@@ -324,18 +324,31 @@ class PanelNodeForCircuits(udi_interface.Node):
 
         for i in range(1, how_many+1):
             LOGGER.debug("\n\tHere is the currentCircuitData:\n\t\t" + allCircuitsArray[i] + "\n")
+            
             current_IPaddress = self.ipAddress
+            LOGGER.debug("\n\t\t'current_IPaddress':\t" + current_IPaddress + "\n")
             current_BearerToken = self.token
+            LOGGER.debug("\n\t\t'current_BearerToken':\t" + current_BearerToken + "\n")
+            
             address = 'S' + panelNumberPrefix + '_Circuit_' + str(i)
             address = getValidNodeAddress(address)
+            LOGGER.debug("\n\t\tCalculated 'address':\t" + address + "\n")
+            
             current_circuitID_tuple = allCircuitsArray[i].partition(',')
             current_circuitID = current_circuitID_tuple[0]
+            LOGGER.debug("\n\t\tCalculated 'current_CircuitID':\t" + current_circuitID + "\n")
             current_circuitID = current_circuitID.replace(chr(34),'')
+            LOGGER.debug("\n\t\tCalculated 'current_CircuitID':\t" + current_circuitID + "\n")
+            
             current_circuitName_tuple = allCircuitsArray[i].partition(chr(34) + 'name' + chr(34) + ':')
             current_circuitName = current_circuitName_tuple[2]
+            LOGGER.debug("\n\t\tCalculated 'current_CircuitName':\t" + current_circuitName + "\n")
             current_circuitName_tuple = current_circuitName.partition(',')
             current_circuitName = current_circuitName_tuple[0]
+            LOGGER.debug("\n\t\tCalculated 'current_CircuitName':\t" + current_circuitName + "\n")
             current_circuitName = current_circuitName.replace(chr(34),'')
+            LOGGER.debug("\n\t\tCalculated 'current_CircuitName':\t" + current_circuitName + "\n")
+            
             title = current_circuitName
             #if len(title)<25:
             #    title = title + ' (' + current_circuitID[-(26-len(title)):] + ')'
