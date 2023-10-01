5269c44a6bbf16ec11f5143d88469d6370f56870
residualimages <51413104+residualimages@users.noreply.github.com>
2023-10-01 09:43:54 -0500

    Update SPAN_circuit.py

--- nodes/SPAN_circuit.py
+++ nodes/SPAN_circuit.py
@@ -36,9 +36,9 @@ class CircuitNode(udi_interface.Node):
             {'driver': 'ST', 'value': 0, 'uom': 73},
             {'driver': 'PULSCNT', 'value': 0, 'uom': 56},
             {'driver': 'CLIEMD', 'value': 0, 'uom': 25},
+            {'driver': 'AWAKE', 'value': 0, 'uom': 25},
             {'driver': 'TIME', 'value': 0, 'uom': 151},
             {'driver': 'TIMEREM', 'value': 'Initializing...', 'uom': 145},
-            {'driver': 'AWAKE', 'value': 1, 'uom': 2},
             {'driver': 'GPV', 'value': '???', 'uom': 145},
             {'driver': 'GV1', 'value': 'N/A', 'uom': 56},
             {'driver': 'GV2', 'value': 'N/A', 'uom': 56},
@@ -186,82 +186,96 @@ class CircuitNode(udi_interface.Node):
         
     def poll(self, polltype):
         if 'shortPoll' in polltype:
-            if self.getDriver('AWAKE') == 1:
-                tokenLastTen = self.token[-10:]
-                LOGGER.debug('\n\tPOLL About to parse {} Circuit node of {}, using token ending in {}'.format(self.circuitID,self.ipAddress,tokenLastTen))
-                designatedCircuitData_tuple = self.allCircuitsData.partition(chr(34) + self.circuitID + chr(34) + ':')
-                designatedCircuitData = designatedCircuitData_tuple[2]
-                designatedCircuitData_tuple = designatedCircuitData.partition('},')
-                designatedCircuitData = designatedCircuitData_tuple[0] + '}'
-        
-                '''
-                spanConnection = http.client.HTTPConnection(self.ipAddress)
-                payload = ''
-                headers = {
-                    "Authorization": "Bearer " + self.token
-                }
-                spanConnection.request("GET", "/api/v1/circuits/" + self.circuitID, payload, headers)
-        
-                designatedCircuitResponse = spanConnection.getresponse()
-                designatedCircuitData = designatedCircuitResponse.read()
-                designatedCircuitData = designatedCircuitData.decode("utf-8")
-                '''
-            
-                LOGGER.debug("\n\tPOLL Circuit Data: \n\t\t" + designatedCircuitData + "\n")
-            
-                if "name" in designatedCircuitData:
-                    designatedCircuitStatus_tuple = designatedCircuitData.partition(chr(34) + "relayState" + chr(34) + ":")
-                    designatedCircuitStatus = designatedCircuitStatus_tuple[2]
-                    designatedCircuitStatus_tuple = designatedCircuitStatus.partition(',')
-                    designatedCircuitStatus = designatedCircuitStatus_tuple[0]
+            tokenLastTen = self.token[-10:]
+            LOGGER.debug('\n\tPOLL About to parse {} Circuit node of {}, using token ending in {}'.format(self.circuitID,self.ipAddress,tokenLastTen))
+            designatedCircuitData_tuple = self.allCircuitsData.partition(chr(34) + self.circuitID + chr(34) + ':')
+            designatedCircuitData = designatedCircuitData_tuple[2]
+            designatedCircuitData_tuple = designatedCircuitData.partition('},')
+            designatedCircuitData = designatedCircuitData_tuple[0] + '}'
     
-                    designatedCircuitInstantPowerW_tuple = designatedCircuitData.partition(chr(34) + "instantPowerW" + chr(34) + ":")
-                    designatedCircuitInstantPowerW = designatedCircuitInstantPowerW_tuple[2]
-                    designatedCircuitInstantPowerW_tuple = designatedCircuitInstantPowerW.partition(',')
-                    designatedCircuitInstantPowerW = designatedCircuitInstantPowerW_tuple[0]
-                    designatedCircuitInstantPowerW = math.ceil(float(designatedCircuitInstantPowerW)*100)/100
-                  
-                    LOGGER.debug("\n\tPOLL about to evaluate Circuit Status (" + designatedCircuitStatus + ") and set CLIEMD appropriately.\n")
-                    if "CLOSED" in designatedCircuitStatus:
-                      self.setDriver('CLIEMD', 2, True, True)
-                    elif "OPEN" in designatedCircuitStatus:
-                      self.setDriver('CLIEMD', 1, True, True)
-                    else:
-                      self.setDriver('CLIEMD', 0, True, True)
-                    
-                    LOGGER.debug("\n\tPOLL About to set ST to " + str(designatedCircuitInstantPowerW) + " for Circuit " + self.circuitID + ".\n")
-                    self.setDriver('ST', abs(designatedCircuitInstantPowerW), True, True)
+            '''
+            spanConnection = http.client.HTTPConnection(self.ipAddress)
+            payload = ''
+            headers = {
+                "Authorization": "Bearer " + self.token
+            }
+            spanConnection.request("GET", "/api/v1/circuits/" + self.circuitID, payload, headers)
+    
+            designatedCircuitResponse = spanConnection.getresponse()
+            designatedCircuitData = designatedCircuitResponse.read()
+            designatedCircuitData = designatedCircuitData.decode("utf-8")
+            '''
+        
+            LOGGER.debug("\n\tPOLL Circuit Data: \n\t\t" + designatedCircuitData + "\n")
+        
+            if "name" in designatedCircuitData:
+                designatedCircuitStatus_tuple = designatedCircuitData.partition(chr(34) + "relayState" + chr(34) + ":")
+                designatedCircuitStatus = designatedCircuitStatus_tuple[2]
+                designatedCircuitStatus_tuple = designatedCircuitStatus.partition(',')
+                designatedCircuitStatus = designatedCircuitStatus_tuple[0]
 
-                    if len(str(designatedCircuitInstantPowerW)) > 0:
-                        nowEpoch = int(time.time())
-                        nowDT = datetime.datetime.fromtimestamp(nowEpoch)
-                        LOGGER.debug("\n\tPOLL about to set TIME and ST; TIME = '" + nowDT.strftime("%m/%d/%Y %H:%M:%S") + "'.\n")
-                        self.setDriver('TIME', nowEpoch, True, True)
-                        self.setDriver('TIMEREM', nowDT.strftime("%m/%d/%Y %H:%M:%S"), False)
-                    else:
-                        LOGGER.warning("\n\tPOLL ERROR: Unable to get designatedCircuitInstantPowerW from designatedCircuitData:\n\t\t" + designatedCircuitData + "\n")
-                        self.setDriver('TIMEREM', "POLL Error Querying" , False)
+                designatedCircuitPriority_tuple = designatedCircuitData.partition(chr(34) + "priority" + chr(34) + ":")
+                designatedCircuitPriority = designatedCircuitPriority_tuple[2]
+                designatedCircuitPriority_tuple = designatedCircuitPriority.partition(',')
+                designatedCircuitPriority = designatedCircuitPriority_tuple[0]                    
+
+                designatedCircuitInstantPowerW_tuple = designatedCircuitData.partition(chr(34) + "instantPowerW" + chr(34) + ":")
+                designatedCircuitInstantPowerW = designatedCircuitInstantPowerW_tuple[2]
+                designatedCircuitInstantPowerW_tuple = designatedCircuitInstantPowerW.partition(',')
+                designatedCircuitInstantPowerW = designatedCircuitInstantPowerW_tuple[0]
+                designatedCircuitInstantPowerW = math.ceil(float(designatedCircuitInstantPowerW)*100)/100
+              
+                LOGGER.debug("\n\tPOLL about to evaluate Circuit Status (" + designatedCircuitStatus + ") and set CLIEMD appropriately.\n")
+                if "CLOSED" in designatedCircuitStatus:
+                  self.setDriver('CLIEMD', 2, True, True)
+                elif "OPEN" in designatedCircuitStatus:
+                  self.setDriver('CLIEMD', 1, True, True)
                 else:
-                    LOGGER.warning("\n\tPOLL Issue getting data for circuit '" + self.circuitID + "'.\n")
-                    self.setDriver('TIMEREM', "Error Querying" , False)
-            else:
-                LOGGER.debug("\n\t\tSkipping POLL query of Circuit node '" + self.circuitID + "' due to AWAKE=0.\n")
-                self.setDriver('TIMEREM', "Not Actively Querying", False)
+                  self.setDriver('CLIEMD', 0, True, True)
+                    
+                LOGGER.debug("\n\tPOLL about to evaluate Circuit Priority (" + designatedCircuitPriority + ") and set MODE appropriately.\n")
+                if "MUST" in designatedCircuitPriority:
+                  self.setDriver('MODE', 3, True, True)
+                elif "NICE" in designatedCircuitPriority:
+                  self.setDriver('MODE', 2, True, True)
+                elif "NON_" in designatedCircuitPriority:
+                  self.setDriver('MODE', 1, True, True)
+                else:
+                  self.setDriver('MODE', 0, True, True)
+                
+                LOGGER.debug("\n\tPOLL About to set ST to " + str(designatedCircuitInstantPowerW) + " for Circuit " + self.circuitID + ".\n")
+                self.setDriver('ST', abs(designatedCircuitInstantPowerW), True, True)
 
-    def toggle_circuit_monitoring(self,val):
-        # On startup this will always go back to true which is the default, but how do we restore the previous user value?
-        LOGGER.debug(f'{self.address} being set via toggle_circuit_monitoring to val={val}')
+                if len(str(designatedCircuitInstantPowerW)) > 0:
+                    nowEpoch = int(time.time())
+                    nowDT = datetime.datetime.fromtimestamp(nowEpoch)
+                    LOGGER.debug("\n\tPOLL about to set TIME and ST; TIME = '" + nowDT.strftime("%m/%d/%Y %H:%M:%S") + "'.\n")
+                    self.setDriver('TIME', nowEpoch, True, True)
+                    self.setDriver('TIMEREM', nowDT.strftime("%m/%d/%Y %H:%M:%S"), False)
+                else:
+                    LOGGER.warning("\n\tPOLL ERROR: Unable to get designatedCircuitInstantPowerW from designatedCircuitData:\n\t\t" + designatedCircuitData + "\n")
+                    self.setDriver('TIMEREM', "POLL Error Querying" , False)
+            else:
+                LOGGER.warning("\n\tPOLL Issue getting data for circuit '" + self.circuitID + "'.\n")
+                self.setDriver('TIMEREM', "Error Querying" , False)
+
+    def update_circuit_status(self,val):
+        LOGGER.warning(f'\n\t\t{self.address} being set via update_circuit_status to val={val}\n')
+        self.setDriver('CLIEMD', val, True, True)
+
+    def cmd_update_circuit_status(self,val):
+        LOGGER.warning(f'\n\t{self.address} being set via cmd_update_circuit_status to val={val}\n')
+        self.update_circuit_status(val)
+
+    def update_circuit_priority(self,val):
+        LOGGER.warning(f'\n\t\t{self.address} being set via update_circuit_priority to val={val}\n')
         self.setDriver('AWAKE', val, True, True)
 
-    def cmd_toggle_circuit_monitoring(self,val):
-        val = self.getDriver('AWAKE')
-        LOGGER.debug(f'{self.address} being set via cmd_toggle_circuit_monitoring to val={val}')
-        if val == 1:
-            val = 0
-        else:
-            val = 1
-        self.toggle_circuit_monitoring(val)
+    def cmd_update_circuit_priority(self,val):
+        LOGGER.warning(f'\n\t{self.address} being set via cmd_update_circuit_priority to val={val}\n')
+        self.update_circuit_priority(val)
 
     commands = {
-        "TOGGLE_CIRCUIT_MONITORING": cmd_toggle_circuit_monitoring,
+        "UPDATE_CIRCUIT_STATUS": cmd_update_circuit_status,
+        "UPDATE_CIRCUIT_PRIORITY": cmd_update_circuit_status,
     }
