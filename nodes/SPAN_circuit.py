b1ea8dc987e657e305e82a828e763a8cfc3dfe4e
residualimages <51413104+residualimages@users.noreply.github.com>
2023-10-01 18:52:13 -0500

    Update SPAN_circuit.py

--- nodes/SPAN_circuit.py
+++ nodes/SPAN_circuit.py
@@ -247,7 +247,7 @@ class CircuitNode(udi_interface.Node):
                   self.setDriver('AWAKE', 0, True, True)
                 
                 LOGGER.debug("\n\tPOLL About to set ST to " + str(designatedCircuitInstantPowerW) + " for Circuit " + self.circuitID + ".\n")
-                self.setDriver('ST', abs(designatedCircuitInstantPowerW), True, True)
+                self.setDriver('ST', int(abs(designatedCircuitInstantPowerW)*100), True, True)
 
                 if len(str(designatedCircuitInstantPowerW)) > 0:
                     nowEpoch = int(time.time())
