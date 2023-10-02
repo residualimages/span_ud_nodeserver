453fc3435f55b662af3abbb6bfd477466ed22256
residualimages <51413104+residualimages@users.noreply.github.com>
2023-10-01 19:31:41 -0500

    Update SPAN_breaker.py

--- nodes/SPAN_breaker.py
+++ nodes/SPAN_breaker.py
@@ -30,6 +30,7 @@ class BreakerNode(udi_interface.Node):
             {'driver': 'PULSCNT', 'value': 0, 'uom': 56},
             {'driver': 'CLIEMD', 'value': 0, 'uom': 25},
             {'driver': 'TIME', 'value': 0, 'uom': 151},
+            {'driver': 'MOON', 'value': -1, 'uom': 56},
             {'driver': 'TIMEREM', 'value': -1, 'uom': 56}
             ]
 
@@ -102,7 +103,8 @@ class BreakerNode(udi_interface.Node):
             nowEpoch = int(time.time())
             nowDT = datetime.datetime.fromtimestamp(nowEpoch)
             self.setDriver('TIME', nowEpoch, True, True)
-            self.setDriver('TIMEREM', nowDT.strftime("%M.%S"), True, True, None, nowDT.strftime("%m/%d/%Y %H:%M:%S"))
+            self.setDriver('MOON', nowDT.strftime("%H.%M"), True, True, None, nowDT.strftime("%m/%d/%Y %H:%M:%S"))
+            self.setDriver('TIMEREM', nowDT.strftime("%S"), True, True, None, nowDT.strftime("%m/%d/%Y %H:%M:%S"))
 
     def wait_for_node_done(self):
         while len(self.n_queue) == 0:
@@ -147,8 +149,9 @@ class BreakerNode(udi_interface.Node):
             nowDT = datetime.datetime.fromtimestamp(nowEpoch)
             
             self.setDriver('TIME', nowEpoch, True, True)
-            self.setDriver('TIMEREM', nowDT.strftime("%M.%S"), True, True, None, nowDT.strftime("%m/%d/%Y %H:%M:%S"))
-            LOGGER.debug("\n\tINIT Final for PULSCNT, TIME, and TIMEREM now complete.\n")
+            self.setDriver('MOON', nowDT.strftime("%H.%M"), True, True, None, nowDT.strftime("%m/%d/%Y %H:%M:%S"))
+            self.setDriver('TIMEREM', nowDT.strftime("%S"), True, True, None, nowDT.strftime("%m/%d/%Y %H:%M:%S"))
+            LOGGER.debug("\n\tINIT Final for PULSCNT, TIME, MOON, and TIMEREM now complete.\n")
         
         self.poll('shortPoll')
         
@@ -203,10 +206,12 @@ class BreakerNode(udi_interface.Node):
                     nowEpoch = int(time.time())
                     nowDT = datetime.datetime.fromtimestamp(nowEpoch)
                     LOGGER.debug("\n\tPOLL about to set TIME and ST; TIME = '" + nowDT.strftime("%m/%d/%Y %H:%M:%S") + "'.\n")
-                    self.setDriver('TIME', nowEpoch, True, True)                            
-                    self.setDriver('TIMEREM', nowDT.strftime("%M.%S"), True, True, None, nowDT.strftime("%m/%d/%Y %H:%M:%S"))
+                    self.setDriver('TIME', nowEpoch, True, True)         
+                    self.setDriver('MOON', nowDT.strftime("%H.%M"), True, True, None, nowDT.strftime("%m/%d/%Y %H:%M:%S"))
+                    self.setDriver('TIMEREM', nowDT.strftime("%S"), True, True, None, nowDT.strftime("%m/%d/%Y %H:%M:%S"))
             else:
                 LOGGER.warning("\n\tPOLL ERROR: Unable to get designatedBreakerInstantPowerW from designatedBreakerData:\n\t\t" + designatedBreakerData + "\n")
+                self.setDriver('MOON', -1, True, True, None, "POLL Error Querying designatedBreakerData")
                 self.setDriver('TIMEREM', -1, True, True, None, "POLL Error Querying designatedBreakerData")
                 
     '''
