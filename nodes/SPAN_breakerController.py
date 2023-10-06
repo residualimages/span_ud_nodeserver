ddc8045f9b84665f9e4814b442fc041e70735321
residualimages <51413104+residualimages@users.noreply.github.com>
2023-10-06 12:26:42 -0500

    Update SPAN_breakerController.py

--- nodes/SPAN_breakerController.py
+++ nodes/SPAN_breakerController.py
@@ -339,7 +339,7 @@ class PanelNodeForBreakers(udi_interface.Node):
                 problemChildren = ''
 
                 breakerCount = len(self.childBreakerNodes)
-                if breakerCount <> 32:
+                if breakerCount != 32:
                     LOGGER.warning("\n\tBREAKER CHILD NODE TRACKING ERROR: Any Breaker Controller Node should be tracking exactly 32 child Breaker Nodes; as it stands right now, controller '" + self.address + "' is tracking " + str(breakerCount) + " child Breaker Nodes.\n")
                 
                 for i in range(1,33):
