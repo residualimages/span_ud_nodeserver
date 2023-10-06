d000d6899c5d32c9ce1909e14a32daf372730f99
residualimages <51413104+residualimages@users.noreply.github.com>
2023-10-06 17:39:42 -0500

    Update SPAN_ctl.py

--- nodes/SPAN_ctl.py
+++ nodes/SPAN_ctl.py
@@ -132,7 +132,7 @@ class Controller(udi_interface.Node):
     until it is fully created before we try to use it.
     '''
     def node_queue(self, data):
-        self.childrenRunng += 1
+        self.childrenRunning += 1
         if data['address'] == self.address:
             LOGGER.debug("\n\tISY Object created under 'controller':\t" + self.ISY._isy_ip + ":" + str(self.ISY._isy_port) + ", which is itself NS #" + str(self.poly.profileNum) + ", and has self.address of '" + str(self.address) + "'.\n")   
             LOGGER.debug("\n\t\tUNAuthorized (expecting this to be false): " + str(self.ISY.unauthorized) + ".\n")
