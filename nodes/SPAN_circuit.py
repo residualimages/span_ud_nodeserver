6bb65fe34c3d133dfd83202417457b04a8f7a0db
residualimages <51413104+residualimages@users.noreply.github.com>
2023-11-18 17:50:32 -0600

    Update SPAN_circuit.py

--- nodes/SPAN_circuit.py
+++ nodes/SPAN_circuit.py
@@ -215,8 +215,10 @@ class CircuitNode(udi_interface.Node):
                     
                     if '<status>200</status>' not in localResponseData:
                         LOGGER.warning("\n\t\tPUSHING REPORT ERROR on '" + self.address + "' for driver " + driver + ": RESPONSE from report was not '<status>200</status>' as expected:\n\t\t\t" + localResponseData + "\n")
-                except:
+                except http.client.HTTPException:
                     LOGGER.error("\n\t\tPUSHING REPORT ERROR on '" + self.address + "' for driver " + driver + " had an ERROR.\n")
+                finally:
+                    localConnection.close()  
         else:
             LOGGER.warning("\n\t\PUSHING REPORT ERROR on '" + self.address + "' for driver " + driver + ": looks like this is a PG3 install but the ISY authorization state seems to currently be 'Unauthorized': 'True'.\n")
     
