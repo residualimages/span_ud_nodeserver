18f92aa1f9d0de8120a4c4905407cf691fbca19c
residualimages <51413104+residualimages@users.noreply.github.com>
2023-12-07 20:51:49 -0600

    Update SPAN_ctl.py

--- nodes/SPAN_ctl.py
+++ nodes/SPAN_ctl.py
@@ -120,7 +120,7 @@ class Controller(udi_interface.Node):
                         self.breakerControllers[i].pollBreakerController(polltype + "|poll passed to '" + self.breakerControllers[i].address + "' from root controller in FOR loop of its own poll")
                         self.pushTextToDriver('GPV',"Last Short Poll Date / Time: " + nowDT.strftime("%m/%d/%Y %I:%M:%S %p"))
                     except:
-                        LOGGER.error("\n\tERROR Handling Breaker Controller #" + i.string + ".\n")
+                        LOGGER.error("\n\tERROR Handling Breaker Controller #" + format(i) + ".\n")
 
             '''
             nodes = self.poly.getNodes()
